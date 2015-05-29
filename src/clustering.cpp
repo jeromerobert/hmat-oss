jeromerobert/hmat-oss

commit 878c93ee910d613b06e24066163fd4a6aca4401b

Remove restriction on 3D
Allow any spatial dimension for coordinates.
Fix handling of group indices.

src/clustering.cpp
@@ -94,8 +94,9 @@ largestDimension(const hmat::ClusterTree& node)
   int maxDim = -1;
   double maxSize = -1.0;
   hmat::AxisAlignedBoundingBox* bbox = getAxisAlignedBoundingbox(node);
-  for (int i = 0; i < 3; i++) {
-    double size = (bbox->bbMax.xyz[i] - bbox->bbMin.xyz[i]);
+  const int dimension = node.data.coordinates()->dimension();
+  for (int i = 0; i < dimension; i++) {
+    double size = (bbox->bbMax[i] - bbox->bbMin[i]);
     if (size > maxSize) {
       maxSize = size;
       maxDim = i;
@@ -109,8 +110,9 @@ volume(const hmat::ClusterTree& node)
 {
   hmat::AxisAlignedBoundingBox* bbox = getAxisAlignedBoundingbox(node);
   double result = 1.;
-  for (int dim = 0; dim < 3; dim++) {
-    result *= (bbox->bbMax.xyz[dim] - bbox->bbMin.xyz[dim]);
+  const int dimension = node.data.coordinates()->dimension();
+  for (int dim = 0; dim < dimension; dim++) {
+    result *= (bbox->bbMax[dim] - bbox->bbMin[dim]);
   }
   return result;
 }
@@ -135,11 +137,11 @@ GeometricBisectionAlgorithm::partition(ClusterTree& current, std::vector<Cluster
   AxisAlignedBoundingBox* bbox = new AxisAlignedBoundingBox(current.data);
   current.clusteringAlgoData_ = bbox;
 
-  double middle = .5 * (bbox->bbMin.xyz[dim] + bbox->bbMax.xyz[dim]);
+  double middle = .5 * (bbox->bbMin[dim] + bbox->bbMax[dim]);
   int middleIndex = 0;
   int* myIndices = current.data.indices() + current.data.offset();
   const double* coord = &current.data.coordinates()->get(0,0);
-  while (coord[myIndices[middleIndex]*3+dim] < middle) {
+  while (coord[myIndices[middleIndex]*spatialDimension_+dim] < middle) {
     middleIndex++;
   }
   if (NULL != current.data.group_index())
@@ -153,12 +155,20 @@ GeometricBisectionAlgorithm::partition(ClusterTree& current, std::vector<Cluster
       int lower = middleIndex-1;
       while (upper < current.data.size() && group_index[upper] == group)
         ++upper;
-      while (lower > 0 && group_index[lower] == group)
+      while (lower >= 0 && group_index[lower] == group)
         --lower;
-      if (lower == 0 || (coord[myIndices[upper]*3+dim] + coord[myIndices[lower]*3+dim] < 2.0 * middle))
+      if (lower < 0 && upper == current.data.size())
+      {
+        // All degrees of freedom belong to the same group, this is fine
+      }
+      else if (lower < 0)
+        middleIndex = upper;
+      else if (upper == current.data.size())
+        middleIndex = lower + 1;
+      else if (coord[myIndices[upper]*spatialDimension_+dim] + coord[myIndices[lower]*spatialDimension_+dim] < 2.0 * middle)
         middleIndex = upper;
       else
-        middleIndex = lower;
+        middleIndex = lower + 1;
     }
   }
   children.push_back(current.slice(current.data.offset(), middleIndex));
@@ -197,12 +207,20 @@ MedianBisectionAlgorithm::partition(ClusterTree& current, std::vector<ClusterTre
       int lower = middleIndex-1;
       while (upper < current.data.size() && group_index[upper] == group)
         ++upper;
-      while (lower > 0 && group_index[lower] == group)
+      while (lower >= 0 && group_index[lower] == group)
         --lower;
-      if (lower == 0 || (upper + lower < 2 * middleIndex))
+      if (lower < 0 && upper == current.data.size())
+      {
+        // All degrees of freedom belong to the same group, this is fine
+      }
+      else if (lower < 0)
+        middleIndex = upper;
+      else if (upper == current.data.size())
+        middleIndex = lower + 1;
+      else if (upper + lower < 2 * middleIndex)
         middleIndex = upper;
       else
-        middleIndex = lower;
+        middleIndex = lower + 1;
     }
   }
   children.push_back(current.slice(current.data.offset(), middleIndex));

src/cluster_tree.cpp
@@ -192,26 +192,42 @@ ClusterTree* ClusterTree::copy(const ClusterTree* copyFather) const {
 }
 
 AxisAlignedBoundingBox::AxisAlignedBoundingBox(const ClusterData& data)
+  : dimension_(data.coordinates()->dimension())
+  , bbMin(new double[dimension_])
+  , bbMax(new double[dimension_])
 {
   int* myIndices = data.indices() + data.offset();
   const double* coord = &data.coordinates()->get(0, 0);
-  bbMin = Point(coord[3*myIndices[0]], coord[3*myIndices[0]+1], coord[3*myIndices[0]+2]);
-  bbMax = Point(coord[3*myIndices[0]], coord[3*myIndices[0]+1], coord[3*myIndices[0]+2]);
+  memcpy(&bbMin[0], &coord[dimension_*myIndices[0]], sizeof(double) * dimension_);
+  memcpy(&bbMax[0], &coord[dimension_*myIndices[0]], sizeof(double) * dimension_);
 
   for (int i = 0; i < data.size(); ++i) {
     int index = myIndices[i];
-    const double* p = &coord[3*index];
-    for (int dim = 0; dim < 3; ++dim) {
-      bbMin.xyz[dim] = std::min(bbMin.xyz[dim], p[dim]);
-      bbMax.xyz[dim] = std::max(bbMax.xyz[dim], p[dim]);
+    const double* p = &coord[dimension_*index];
+    for (int dim = 0; dim < dimension_; ++dim) {
+      bbMin[dim] = std::min(bbMin[dim], p[dim]);
+      bbMax[dim] = std::max(bbMax[dim], p[dim]);
     }
   }
+}
+
+AxisAlignedBoundingBox::~AxisAlignedBoundingBox()
+{
+  delete [] bbMin;
+  delete [] bbMax;
 }
 
 double
 AxisAlignedBoundingBox::diameter() const
 {
-  return bbMax.distanceTo(bbMin);
+  double result = 0.0;
+  for(int i = 0; i < dimension_; ++i)
+  {
+    double delta = bbMin[i] - bbMax[i];
+    result += delta * delta;
+  }
+
+  return sqrt(result);
 }
 
 double
@@ -220,20 +236,13 @@ AxisAlignedBoundingBox::distanceTo(const AxisAlignedBoundingBox& other) const
   double result = 0.;
   double difference = 0.;
 
-  difference = std::max(0., bbMin.xyz[0] - other.bbMax.xyz[0]);
-  result += difference * difference;
-  difference = std::max(0., other.bbMin.xyz[0] - bbMax.xyz[0]);
-  result += difference * difference;
-
-  difference = std::max(0., bbMin.xyz[1] - other.bbMax.xyz[1]);
-  result += difference * difference;
-  difference = std::max(0., other.bbMin.xyz[1] - bbMax.xyz[1]);
-  result += difference * difference;
-
-  difference = std::max(0., bbMin.xyz[2] - other.bbMax.xyz[2]);
-  result += difference * difference;
-  difference = std::max(0., other.bbMin.xyz[2] - bbMax.xyz[2]);
-  result += difference * difference;
+  for(int i = 0; i < dimension_; ++i)
+  {
+    difference = std::max(0., bbMin[i] - other.bbMax[i]);
+    result += difference * difference;
+    difference = std::max(0., other.bbMin[i] - bbMax[i]);
+    result += difference * difference;
+  }
 
   return sqrt(result);
 }

src/cluster_tree.hpp
@@ -203,10 +203,14 @@ class AxisAlignedBoundingBox
 {
 public:
   explicit AxisAlignedBoundingBox(const ClusterData& node);
-  Point bbMin, bbMax;
+  ~AxisAlignedBoundingBox();
 
   double diameter() const;
   double distanceTo(const AxisAlignedBoundingBox& other) const;
+private:
+  const int dimension_;
+public:
+  double *bbMin, *bbMax;
 };
 
 }  // end namespace hmat

src/h_matrix.cpp
@@ -1286,7 +1286,8 @@ void HMatrix<T>::dumpTreeToFile(const char* filename) const {
   ofstream file;
   const DofCoordinates* points = rows()->coordinates();
   const double* coord = &points->get(0, 0);
-  int* indices = rows()->indices();
+  const int* indices = rows()->indices();
+  const int dimension = points->dimension();
   string delimiter;
 
   file.open(filename);
@@ -1295,8 +1296,11 @@ void HMatrix<T>::dumpTreeToFile(const char* filename) const {
        << "  \"points\": [" << endl;
   delimiter = "";
   for (int i = 0; i < points->size(); i++) {
-    file << "    " << delimiter << "[" << coord[3*i] << ", " << coord[3*i+1] << ", " << coord[3*i+2]
-         << "]" << endl;
+    file << "    " << delimiter << "[" << coord[dimension*i];
+    for (int dim = 0; dim < dimension; ++dim) {
+      file << ", " << coord[dimension*i+dim];
+    }
+    file << "]" << endl;
     delimiter = " ,";
   }
   // Mapping
@@ -1321,18 +1325,33 @@ void HMatrix<T>::dumpSubTree(ofstream& f, int depth) const {
   }
   AxisAlignedBoundingBox rows_bbox(data.rows->data);
   AxisAlignedBoundingBox cols_bbox(data.cols->data);
+  const int rows_dimension(data.rows->data.coordinates()->dimension());
+  const int cols_dimension(data.cols->data.coordinates()->dimension());
 
   f << prefix << "{\"isLeaf\": " << (isLeaf() ? "true" : "false") << "," << endl
     << prefix << " \"depth\": " << depth << "," << endl
     << prefix << " \"rows\": " << "{\"offset\": " << rows()->offset() << ", \"n\": " << rows()->size() << ", "
-    << "\"boundingBox\": [[" << rows_bbox.bbMin.x << ", " << rows_bbox.bbMin.y << ", " << rows_bbox.bbMin.z << "], ["
-    << rows_bbox.bbMax.x << ", " << rows_bbox.bbMax.y << ", " << rows_bbox.bbMax.z << "]]},"
-    << endl
+    << "\"boundingBox\": [[" << rows_bbox.bbMin[0];
+  for (int dim = 1; dim < rows_dimension; ++dim) {
+    f << ", " << rows_bbox.bbMin[dim];
+  }
+  f << "], [" << rows_bbox.bbMax[0];
+  for (int dim = 1; dim < rows_dimension; ++dim) {
+    f << ", " << rows_bbox.bbMax[dim];
+  }
+  f << "]]}," << endl
     << prefix << " \"cols\": " << "{\"offset\": " << cols()->offset() << ", \"n\": " << cols()->size() << ", "
-    << "\"boundingBox\": [[" << cols_bbox.bbMin.x << ", " << cols_bbox.bbMin.y << ", " << cols_bbox.bbMin.z << "], ["
-    << cols_bbox.bbMax.x << ", " << cols_bbox.bbMax.y << ", " << cols_bbox.bbMax.z << "]]},";
+    << "\"boundingBox\": [[" << cols_bbox.bbMin[0];
+  for (int dim = 1; dim < cols_dimension; ++dim) {
+    f << ", " << cols_bbox.bbMin[dim];
+  }
+  f << "], [" << cols_bbox.bbMax[0];
+  for (int dim = 1; dim < cols_dimension; ++dim) {
+    f << ", " << cols_bbox.bbMax[dim];
+  }
+  f << "]]}," << endl;
   if (!isLeaf()) {
-    f << endl << prefix << " \"children\": [" << endl;
+    f << prefix << " \"children\": [" << endl;
     string delimiter("");
     for (int i = 0; i < 4; i++) {
       const HMatrix<T>* child = static_cast<const HMatrix<T>*>(getChild(i));
@@ -1346,13 +1365,13 @@ void HMatrix<T>::dumpSubTree(ofstream& f, int depth) const {
   } else {
     // It's a leaf
     if (isFullMatrix()) {
-      f << endl << prefix << " \"leaf_type\": \"Full\"";
+      f << prefix << " \"leaf_type\": \"Full\"";
     } else if (isRkMatrix()) {
-      f << endl << prefix << " \"leaf_type\": \"Rk\", \"k\": " << data.rk->k << ",";
+      f << prefix << " \"leaf_type\": \"Rk\", \"k\": " << data.rk->k << ",";
       // f << endl << prefix << " \"eta\": " << this->data.rows->getEta(this->data.cols) << ",";
-      f << endl << prefix << " \"method\": " << this->data.rk->method;
+      f << prefix << " \"method\": " << this->data.rk->method;
     } else {
-      f << endl << prefix << " \"leaf_type\": \"N/A\"";
+      f << prefix << " \"leaf_type\": \"N/A\"";
     }
   }
   f << "}";

commit f3b2cedfd5e0a034f3bc67e6be06f2a2dcb830b7

Fix GeometricBisectionAlgorithm::partition
It had been broken by 878c93e.

src/clustering.cpp
@@ -109,11 +109,12 @@ void
 GeometricBisectionAlgorithm::partition(ClusterTree& current, std::vector<ClusterTree*>& children) const
 {
   int dim;
+  if (spatialDimension_ < 0) {
+    spatialDimension_ = current.data.coordinates()->dimension();
+  }
   if (axisIndex_ < 0) {
     dim = largestDimension(current);
   } else {
-    if (spatialDimension_ < 0)
-      spatialDimension_ = current.data.coordinates()->dimension();
     dim = ((axisIndex_ + current.depth) % spatialDimension_);
   }
   sortByDimension(current, dim);