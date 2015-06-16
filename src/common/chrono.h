3ba1983baf76791b7bfe938addd46c493ec183c3
jschueller <julien.schueller@gmail.com>
2015-06-16 13:45:51 +0200

    Avoid macro redefinitions with mingw

--- src/common/chrono.h
+++ src/common/chrono.h
@@ -37,8 +37,12 @@
 #include <sys/resource.h>
 #endif
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 
