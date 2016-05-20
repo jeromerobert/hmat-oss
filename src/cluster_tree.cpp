624f2534d753376c343680bab14742b06fbeab08
Denis Barbier <bouzim@gmail.com>
2016-05-20 17:34:20 +0200

    Fix compilation with icc 13

--- src/cluster_tree.cpp
+++ src/cluster_tree.cpp
@@ -110,7 +110,7 @@ DofData::copy() const
 }
 
 ClusterTree::ClusterTree(const DofData* dofData)
-  : Tree(NULL)
+  : Tree<ClusterTree>(NULL)
   , data(dofData)
   , clusteringAlgoData_(NULL)
   , admissibilityAlgoData_(NULL)
@@ -118,7 +118,7 @@ ClusterTree::ClusterTree(const DofData* dofData)
 }
 
 ClusterTree::ClusterTree(const ClusterTree& other)
-  : Tree(NULL)
+  : Tree<ClusterTree>(NULL)
   , data(other.data)
   , clusteringAlgoData_(NULL)
   , admissibilityAlgoData_(NULL)
