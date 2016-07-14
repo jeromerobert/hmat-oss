jeromerobert/hmat-oss

commit b08002dd3d36d0415d8b20f5404dd5870f3a31f8

Fix compilation with glibc 2.22 isnan
Also remove mingw and apple tests which are more likely replaced by
the __cplusplus test.

src/compression.cpp
@@ -48,7 +48,9 @@
 # ifndef __INTEL_COMPILER
 #  define isnan _isnan
 # endif
-#elif defined(__MINGW32__) || defined(__APPLE__) || __cplusplus > 201100L
+#elif __GLIBC__ == 2 && __GLIBC_MINOR__ < 23
+// https://sourceware.org/bugzilla/show_bug.cgi?id=19439
+#elif __cplusplus > 201103L
 using std::isnan;
 #endif
 

commit db8039dae6fe2cbaa72d8badb3d32278bf2166e5

Fix compilation with old mingw compilers
* ::isnan seems to only be available in glibc, so we support only c++11
or greater compilers or glibc
* This should be a better (more general fix) for fea0f51
* This was broken by b08002d

src/compression.cpp
@@ -50,7 +50,7 @@
 # endif
 #elif __GLIBC__ == 2 && __GLIBC_MINOR__ < 23
 // https://sourceware.org/bugzilla/show_bug.cgi?id=19439
-#elif __cplusplus > 201103L || __APPLE__
+#elif __cplusplus > 201103L || !defined(__GLIBC__)
 using std::isnan;
 #endif
 