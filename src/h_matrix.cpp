b68a45311c452f5a5b6678795dc010d5cb0360d7
Jerome Robert <jeromerobert@gmx.com>
2016-10-05 11:29:16 +0200

    Ignore blocks of size 0 in the statistics

--- src/h_matrix.cpp
+++ src/h_matrix.cpp
@@ -352,22 +352,26 @@ void HMatrix<T>::assembleSymmetric(Assembly<T>& f,
 
 template<typename T> void HMatrix<T>::info(hmat_info_t & result) {
     result.nr_block_clusters++;
-    if(this->isLeaf()) {
-        size_t s = ((size_t)rows()->size()) * cols()->size();
+    int r = rows()->size();
+    int c = cols()->size();
+    if(r == 0 || c == 0) {
+        return;
+    } else if(this->isLeaf()) {
+        size_t s = ((size_t)r) * c;
         result.uncompressed_size += s;
         if(isRkMatrix()) {
-            size_t mem = rank() * (((size_t)rows()->size()) + cols()->size());
+            size_t mem = rank() * (((size_t)r) + c);
             result.compressed_size += mem;
             int dim = result.largest_rk_dim_cols + result.largest_rk_dim_rows;
             if(rows()->size() + cols()->size() > dim) {
-                result.largest_rk_dim_cols = cols()->size();
-                result.largest_rk_dim_rows = rows()->size();
+                result.largest_rk_dim_cols = c;
+                result.largest_rk_dim_rows = r;
             }
 
             size_t old_s = ((size_t)result.largest_rk_mem_cols + result.largest_rk_mem_rows) * result.largest_rk_mem_rank;
             if(mem > old_s) {
-                result.largest_rk_mem_cols = cols()->size();
-                result.largest_rk_mem_rows = rows()->size();
+                result.largest_rk_mem_cols = c;
+                result.largest_rk_mem_rows = r;
                 result.largest_rk_mem_rank = rank();
             }
             result.rk_count++;
