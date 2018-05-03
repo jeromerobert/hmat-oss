4b455cbb7fc156d01a62a8d20bee2a0e965ad6b7
Denis Barbier <bouzim@gmail.com>
2018-05-03 18:19:17 +0200

    Minor changes

--- src/c_wrapping.hpp
+++ src/c_wrapping.hpp
@@ -70,28 +70,26 @@ void assemble_generic(hmat_matrix_t* matrix, hmat_assemble_context_t * ctx) {
         HMAT_ASSERT(ctx->block_compute == NULL && ctx->advanced_compute == NULL && ctx->simple_compute == NULL);
         hmat::Assembly<T> * cppAssembly = (hmat::Assembly<T> *)ctx->assembly;
         hmat->assemble(*cppAssembly, sf, ctx->progress);
-        if(!assembleOnly)
-            hmat->factorize(ctx->factorization, ctx->progress);
     } else if(ctx->block_compute != NULL || ctx->advanced_compute != NULL) {
         HMAT_ASSERT(ctx->simple_compute == NULL && ctx->assembly == NULL);
+        HMAT_ASSERT(ctx->prepare != NULL);
         hmat::AssemblyFunction<T, hmat::BlockFunction> * f =
             new hmat::AssemblyFunction<T, hmat::BlockFunction> (
             hmat::BlockFunction<T>(hmat->rows(), hmat->cols(),
             ctx->user_context, ctx->prepare,
             ctx->block_compute, ctx->advanced_compute));
         hmat->assemble(*f, sf, true, ctx->progress, true);
-        if(!assembleOnly)
-            hmat->factorize(ctx->factorization, ctx->progress);
     } else if(ctx->simple_compute != NULL) {
         HMAT_ASSERT(ctx->block_compute == NULL && ctx->advanced_compute == NULL && ctx->assembly == NULL);
         hmat::AssemblyFunction<T, hmat::SimpleFunction> * f =
             new hmat::AssemblyFunction<T, hmat::SimpleFunction>(
             hmat::SimpleFunction<T>(ctx->simple_compute, ctx->user_context));
         hmat->assemble(*f, sf, true, ctx->progress, true);
-        if(!assembleOnly)
-            hmat->factorize(ctx->factorization, ctx->progress);
     } else
       HMAT_ASSERT_MSG(0, "No valid assembly method in assemble_generic()");
+
+    if(!assembleOnly)
+        hmat->factorize(ctx->factorization, ctx->progress);
 }
 
 template<typename T, template <typename> class E>
