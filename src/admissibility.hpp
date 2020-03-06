bae81a362099bb10be422141667cae974e4b750c
Denis Barbier <bouzim@gmail.com>
2020-03-06 15:54:43 +0100

    New ProxyAdmissibilityCondition class
    This abstract class delegates all methods to another AdmissibilityCondition
    instance.  It may be useful when an admissibility condition needs to override
    only few methods of another class.

--- src/admissibility.hpp
+++ src/admissibility.hpp
@@ -180,5 +180,49 @@ public:
     void never(bool n);
     static void setBlockSizeDetector(BlockSizeDetector * b) { blockSizeDetector_ = b; }
 };
+
+/**
+ * @brief Abstract class which can be used as a base class to override only some methods.
+ *        It is abstract because str method is intentionnally not defined.
+ * @param admissibility All methods which are not redefined are delegated to this instance.
+ *        The new object takes ownership of this pointer, caller must not free it.
+ */
+class ProxyAdmissibilityCondition : public AdmissibilityCondition
+{
+public:
+  ProxyAdmissibilityCondition(AdmissibilityCondition * admissibility) : admissibility_(admissibility) {}
+  ~ProxyAdmissibilityCondition() { delete admissibility_; }
+  bool isLowRank(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->isLowRank(rows, cols);
+  }
+  bool stopRecursion(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->stopRecursion(rows, cols);
+  }
+  bool forceRecursion(const ClusterTree& rows, const ClusterTree& cols, size_t elemSize) const {
+    return admissibility_->forceRecursion(rows, cols, elemSize);
+  }
+  bool forceFull(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->forceFull(rows, cols);
+  }
+  bool forceRk(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->forceRk(rows, cols);
+  }
+  std::pair<bool, bool> splitRowsCols(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->splitRowsCols(rows, cols);
+  }
+  bool isInert(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->isInert(rows, cols);
+  }
+  int getApproximateRank(const ClusterTree& rows, const ClusterTree& cols) const {
+    return admissibility_->getApproximateRank(rows, cols);
+  }
+  void clean(const ClusterTree& current) const {
+    return admissibility_->clean(current);
+  }
+
+protected:
+  AdmissibilityCondition * admissibility_;
+};
+
 } //  end namespace hmat
 #endif
