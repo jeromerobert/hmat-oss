06e5f4474328188cbc4ced5f6f794daafabf4382
Denis Barbier <bouzim@gmail.com>
2020-03-09 11:30:26 +0100

    Fix needed for inert blocks

--- src/rk_matrix.cpp
+++ src/rk_matrix.cpp
@@ -1021,7 +1021,7 @@ template<typename T> void RkMatrix<T>::gemmRk(double epsilon, char transHA, char
   while (!ha->isLeaf())
   {
     if (ha->nrChildRow() >= 2 && ha->nrChildCol() >= 2) {
-      if (ha->get(0, 0)->rows()->size() == 0 && ha->get(0, 0)->cols()->size() == 0)
+      if (ha->get(0, 0) && ha->get(0, 0)->rows()->size() == 0 && ha->get(0, 0)->cols()->size() == 0)
       {
         ha = ha->get(1, 1);
         continue;
@@ -1037,7 +1037,7 @@ template<typename T> void RkMatrix<T>::gemmRk(double epsilon, char transHA, char
   while (!hb->isLeaf())
   {
     if (hb->nrChildRow() >= 2 && hb->nrChildCol() >= 2) {
-      if (hb->get(0, 0)->rows()->size() == 0 && hb->get(0, 0)->cols()->size() == 0)
+      if (hb->get(0, 0) && hb->get(0, 0)->rows()->size() == 0 && hb->get(0, 0)->cols()->size() == 0)
       {
         hb = hb->get(1, 1);
         continue;
