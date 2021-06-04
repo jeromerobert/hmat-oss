jeromerobert/hmat-oss

commit c22431a621d44c91db24bd9573f05fa05a9f5447

Fix HMatrix::coarsen with inert blocks

src/h_matrix.cpp
@@ -637,7 +637,7 @@ bool HMatrix<T>::coarsen(double epsilon, HMatrix<T>* upper, bool force) {
   // leaves. Note that this operation could be used hierarchically.
 
   bool allRkLeaves = true;
-  const RkMatrix<T>* childrenArray[this->nrChild()];
+  const RkMatrix<T>* childrenArray[this->nrChild()]={};
   size_t childrenElements = 0;
   for (int i = 0; i < this->nrChild(); i++) {
     HMatrix<T> *child = this->getChild(i);

commit 54d5b3ba45bc89b1182f50ad02c7d2d893404996

Fix compilation with clang
Broken by c22431a (i.e. previous commit)

src/h_matrix.cpp
@@ -637,9 +637,10 @@ bool HMatrix<T>::coarsen(double epsilon, HMatrix<T>* upper, bool force) {
   // leaves. Note that this operation could be used hierarchically.
 
   bool allRkLeaves = true;
-  const RkMatrix<T>* childrenArray[this->nrChild()]={};
+  const RkMatrix<T>* childrenArray[this->nrChild()];
   size_t childrenElements = 0;
   for (int i = 0; i < this->nrChild(); i++) {
+    childrenArray[i] = nullptr;
     HMatrix<T> *child = this->getChild(i);
     if (!child) continue;
     if (!child->isRkMatrix()) {