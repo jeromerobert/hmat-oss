267e681f7a5e103ff95b6613a81ff7d32b25a94e
Emilien Despres <despres@imacs-polytechnique.fr>
2024-12-19 11:38:41 +0100

    Descend clusterTrees by checking superset rather than depth
    It was made for when the corresponding cluster trees are the same,
    which is mathematically what we want, but too restrictive.
    e.g. what if there are some "level0" shenanigans in the cluster tree ?
    
    WIth this commit:
    
    If the clustertrees are the same, it's the same as checking cluster depth.
    
    In more general cases, it does something not dumb rather than looking at
    a non-information.
    
    TODO : what would be a better condition to descend in the general case ?
    - Being a strict superset ?
    - Having some spillage without necessarily containing all of the
      (allegedly) smaller matrix ?
    - Checking the children rather than the parent (goes with the intersect check) ?

--- src/h_matrix.cpp
+++ src/h_matrix.cpp
@@ -1383,31 +1383,31 @@ HMatrix<T>::recursiveGemm(char transA, char transB, T alpha, const HMatrix<T>* a
       // If we assume than the matrices use the same clusterTrees on related rows/cols
       // Try to match the clusterTree depths before going to uncompatibleGemm
       // There are three dimensions, this->rows, this->cols and k (a->cols and b->rows in the absence of transpositions)
-      int a_rdepth = transA == 'N' ? a->rows_->depth : a->cols_->depth;
-      int a_kdepth = transA == 'N' ? a->cols_->depth : a->rows_->depth;
-      int b_cdepth = transB == 'N' ? b->cols_->depth : b->rows_->depth;
-      int b_kdepth = transB == 'N' ? b->rows_->depth : b->cols_->depth;
-      int c_rdepth = this->rows_->depth;
-      int c_cdepth = this->cols_->depth;
-      if (!a->isLeaf() && (a_rdepth < c_rdepth || a_kdepth < b_kdepth)) {
+      const ClusterData* a_r = transA == 'N' ? a->rows() : a->cols();
+      const ClusterData* a_k = transA == 'N' ? a->cols() : a->rows();
+      const ClusterData* b_c = transB == 'N' ? b->cols() : b->rows();
+      const ClusterData* b_k = transB == 'N' ? b->rows() : b->cols();
+      const ClusterData* c_r = this->rows();
+      const ClusterData* c_c = this->cols();
+      if (!a->isLeaf() && (a_r->isStrictSuperSet(*c_r) || a_k->isStrictSuperSet(*b_k))) {
         for (int i = 0; i < a->nrChild(); i++) {
           const HMatrix<T> * childA = a->getChild(i);
-          if (((transA == 'N' ? childA->cols() : childA->rows())->intersects(*(transB == 'N' ? b->rows() : b->cols()))) &&
-              ((transA == 'N' ? childA->rows() : childA->cols())->intersects(*(this->rows()))))
+          if (((transA == 'N' ? childA->cols() : childA->rows())->intersects(*b_k)) &&
+              ((transA == 'N' ? childA->rows() : childA->cols())->intersects(*c_r)))
             this->gemm(transA, transB, alpha, childA, b, 1);
         }
-      } else if (!b->isLeaf() && (b_cdepth < c_cdepth || b_kdepth < a_kdepth)) {
+      } else if (!b->isLeaf() && (b_c->isStrictSuperSet(*c_c) || b_k->isStrictSuperSet(*a_k))) {
         for (int i = 0; i < b->nrChild(); i++) {
           const HMatrix<T> * childB = b->getChild(i);
-          if (((transB == 'N' ? childB->rows() : childB->cols())->intersects(*(transA == 'N' ? a->cols() : a->rows()))) &&
-              ((transB == 'N' ? childB->cols() : childB->rows())->intersects(*(this->cols()))))
+          if (((transB == 'N' ? childB->rows() : childB->cols())->intersects(*a_k)) &&
+              ((transB == 'N' ? childB->cols() : childB->rows())->intersects(*c_c)))
             this->gemm(transA, transB, alpha, a, childB, 1);
         }
-      } else if (!this->isLeaf() && (c_rdepth < a_rdepth || c_cdepth < b_cdepth)) {
+      } else if (!this->isLeaf() && (c_r->isStrictSuperSet(*a_r) || c_c->isStrictSuperSet(*b_c))) {
         for (int i = 0; i < this->nrChild(); i++) {
           HMatrix<T> * childC = this->getChild(i);
-          if (((transA == 'N' ? a->rows() : a->cols())->intersects(*(childC->rows()))) &&
-              ((transB == 'N' ? b->cols() : b->rows())->intersects(*(childC->cols()))))
+          if ((childC->rows()->intersects(*a_r)) &&
+              (childC->cols()->intersects(*b_c)))
             childC->gemm(transA, transB, alpha, a, b, 1);
         }
       }
