23ec3ab7883914784c48bebd75113e30b26fac45
Emilien Despres <despres@imacs-polytechnique.fr>
2026-01-23 09:22:07 +0100

    typo

--- src/h_matrix.cpp
+++ src/h_matrix.cpp
@@ -1338,7 +1338,7 @@ std::tuple<bool ,bool , bool> HMatrix<T>::computeGemmRecursion(char transA, char
       dig_b = !b->isLeaf() && cluster_a->isSubset(*cluster_b);
     } else {
       // keep A.rows (resp.B.cols) == C.rows (resp C.cols)
-      // we can't care force A.cols==B.rows as it would be too much a constraint on the structure
+      // we can't force A.cols==B.rows as it would be too much a constraint on the structure
       dig_a = !a->isLeaf() && (row_a==1 || (dig_c && row_a==row_c));
       dig_b = !b->isLeaf() && (col_b==1 || (dig_c && col_b==col_c));
     }
